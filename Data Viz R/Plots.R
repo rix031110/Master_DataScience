7bfd530a459c5711d97fcd98348e70f13b736442
rix031110 <ricdo03@gmail.com>
2026-02-23 16:57:55 +0100

    d

--- Data Viz R/Plots.R
+++ Data Viz R/Plots.R
@@ -10,4 +10,16 @@ ggplot(mpg) +
 ggplot(mpg) + 
   geom_point(mapping = aes(x=displ, y=hwy, shape=drv)) #the shape correspond to the drv
 
+ggplot(mpg) + 
+  geom_smooth(mapping= aes(x=displ, y=hwy, linetype= drv)) 
+#in this case there is a line with a variable ina  plot with other 2. It gives also the
+# variability
 
+############# YOU CAN SUPERIMPOSE ###########à
+ggplot(mpg) + 
+  geom_smooth(mapping= aes(x=displ, y=hwy))+
+  geom_point(mapping = aes(x=displ, y=hwy))
+# If we put the aes argument at the top I don't need to recall it
+ggplot(mpg,mapping= aes(x=displ, y=hwy)) + 
+  geom_smooth()+
+  geom_point(mapping = aes(col= class))
