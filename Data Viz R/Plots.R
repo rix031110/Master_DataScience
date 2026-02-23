aa51ee73b823a2ac242d30507e6b4ec2b371319c
rix031110 <ricdo03@gmail.com>
2026-02-23 16:32:59 +0100

    Lecture

--- Data Viz R/Plots.R
+++ Data Viz R/Plots.R
@@ -4,4 +4,6 @@ mpg
 ggplot(mpg) + #it ccreates a hollow plot
   geom_point(mapping = aes(x=displ, y=hwy)) #type of plot wanted, aes is the estetic part
                                             #with variables
+ggplot(mpg) + 
+  geom_point(mapping = aes(x=displ, y=hwy, color=class)) #the collor correspond to the class
 
