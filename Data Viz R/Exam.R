5375818b97b10680472d4688a1ab18cbf6315bf0
rix031110 <ricdo03@gmail.com>
2026-02-25 19:46:58 +0100

    tries

--- Data Viz R/Exam.R
+++ Data Viz R/Exam.R
@@ -12,5 +12,17 @@ pokemon_df <- tuesdata$pokemon_df
 
 pokemon_df <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-04-01/pokemon_df.csv')
 
+summary(pokemon_df)
+pokemon_df
+library(skimr)
+library(tidyverse)
+skim(pokemon_df)
+
+rock_weight= pokemon_df |> 
+  filter(type_1=="rock") |> 
+  select(weight)
 
 pokemon_df
+library(ggplot2)
+ggplot(pokemon_df)+
+  geom_point(mapping= aes(x= special_attack, y=special_defense, col=type_1))
--- Data Viz R/Lecture2.R
+++
@@ -1,8 +0,0 @@
-library("patchwork")
-library("skimr")
-skim("Desktop/Master_DataScience/Data Viz R/Wholesale_customers_data.csv")
-library("tidyverse")
-library(ggplot2)
-
-
-
